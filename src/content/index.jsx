4fc851041d51e6d71a086cd79bfa30c72e1d8ae4
qzw881130 <qzw881130@gmail.com>
2024-07-02 20:10:18 +0800

    dev

--- src/content/index.jsx
+++ src/content/index.jsx
@@ -16,26 +16,26 @@ console.log('[content] loaded ')
 //         data: {a:3, b: 5}
 //     })
 // });
+if(window.location.pathname.includes('/p/t_pc/course_pc_detail/video/v_')){
+    window.onload = ()=>{
+        console.log('[content] onload2222')
 
-window.onload = ()=>{
-    console.log('[content] onload2222')
+        var checkLoad = () => {
+            console.log('checkLoad called')
+            var timer = setInterval(()=>{
+                var v = document.querySelector('.content_switch_tab');
+                console.log('check .content_switch_tab element... r= ', v);
+                if(!v) return;    
+                playNextVideo();
+                clearInterval(timer);
+            }, 500);
+        
+        }
+        
+        setTimeout(checkLoad, 2000);
 
-    var checkLoad = () => {
-        console.log('checkLoad called')
-        var timer = setInterval(()=>{
-            var v = document.querySelector('.content_switch_tab');
-            console.log('check .content_switch_tab element... r= ', v);
-            if(!v) return;    
-            playNextVideo();
-            clearInterval(timer);
-        }, 500);
-    
+        
     }
-    
-    setTimeout(checkLoad, 2000);
-
-    
 }
-
 
 export {}
